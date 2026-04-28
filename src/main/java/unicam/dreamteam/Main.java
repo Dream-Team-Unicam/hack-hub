e8527db5c56bb7aaa8cf6c00c840fef50d64f48c
Maxim Cognigni <maxim.cognigni@studenti.unicam.it>
2026-04-28 15:10:19 +0200

    + Aggiunta progetto iniziale

---
+++ src/main/java/unicam/dreamteam/Main.java
@@ -0,0 +1,10 @@
+package unicam.dreamteam;
+
+import org.springframework.boot.SpringApplication;
+import unicam.dreamteam.application.HackHubApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(HackHubApplication.class, args);
+    }
+}
---
+++ src/main/java/unicam/dreamteam/application/HackHubApplication.java
@@ -0,0 +1,7 @@
+package unicam.dreamteam.application;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackHubApplication {
+}
